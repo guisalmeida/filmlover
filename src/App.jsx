e0e71b3e4f820b1d6c8c84b31fb8a5ea044ab92f
“guisalmeida” <guisalmeida.dev@gmail.com>
2023-07-29 00:16:48 +0100

    feat: start fetching tmdb data

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,44 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
 import Header from './components/header/header'
 import Footer from './components/footer/footer'
+import MovieCard from './components/movieCard/movieCard'
 
 export default function App() {
+  const API_URL = 'https://api.themoviedb.org/3'
+  const IMAGE_URL = 'http://image.tmdb.org/t/p/w342'
+  const API_KEY = import.meta.env.VITE_API_KEY
+
+  const [movies, setMovies] = useState([])
+
+  const fetchMovies = async () => {
+    const { data } = await axios.get(`${API_URL}/discover/movie?`, {
+      params: {
+        api_key: API_KEY
+      }
+    })
+
+    setMovies(data.results)
+  }
+
+  useEffect(() => {
+    fetchMovies()
+  }, [])
+
   return (
     <>
-      <Header/>
-      <h1>FilmLover</h1>
-      <Footer/>
+      <Header />
+      <div className='container'>
+        {movies.map(movie => (
+          <MovieCard
+            key={movie.id}
+            movie={movie}
+            image={IMAGE_URL + movie.poster_path}
+          />
+        ))}
+      </div>
+      <Footer />
     </>
   )
 }
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,15 +1,15 @@
-import Logo from '../../assets/filmlover.svg'
+import Logo from '../../assets/logo_h.svg'
 import User from '../../assets/user.jpeg'
 import * as Styled from './header.styled.js'
 
 export default function header() {
   return (
     <Styled.Header>
-        <a href="#" target="_blank" rel="noopener noreferrer">
-            <img src={Logo} className="logo" alt="Filmlover logo" />
-        </a>
+      <a href="#" target="_blank" rel="noopener noreferrer">
+        <img src={Logo} className="logo" alt="Filmlover logo" />
+      </a>
 
-        <img src={User} className="user" alt="User photo" />
+      <img src={User} className="user" alt="User photo" />
     </Styled.Header>
   )
 }
