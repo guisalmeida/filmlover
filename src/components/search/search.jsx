3a13934ee01c8da9e448d2a9e2d463906fd3a290
“guisalmeida” <guisalmeida.dev@gmail.com>
2023-08-01 12:28:16 +0100

    feat: create search component

--- src/components/search/search.jsx
+++ src/components/search/search.jsx
@@ -1,16 +1,23 @@
 import { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { setMovies } from '../../redux/actions/moviesAction'
 import * as Styled from './search.styled.js'
+import { fetchMovies } from '../../utils/api'
 
-export default function search({ fetchMovies }) {
+export default function Search() {
   const [searchKey, setSearchKey] = useState("")
-  const searchMovies = (e) => {
+  const dispatch = useDispatch()
+
+  const searchMovies = async (e) => {
     e.preventDefault()
-    fetchMovies(searchKey)
+    const searchMovies = await fetchMovies(searchKey)
+    dispatch(setMovies(searchMovies))
+    setSearchKey("")
   }
 
   return (
     <form onSubmit={searchMovies}>
-      <input type="text" onChange={(e) => setSearchKey(e.target.value)} />
+      <input type="text" value={searchKey} onChange={(e) => setSearchKey(e.target.value)} />
       <button type="submit">
         <Styled.SearchIcon />
       </button>
