23bd967837ace7aeb9e2e617039a2b2304d365ce
“guisalmeida” <guisalmeida.dev@gmail.com>
2023-08-10 17:02:26 +0100

    refactor: remove old import

--- src/routes/home/home.jsx
+++ src/routes/home/home.jsx
@@ -1,5 +1,5 @@
 import { useSelector } from 'react-redux'
-import MovieCarousel from '../../components/movieCarousel/movieCarousel'
+
 import CardsCarousel from '../../components/cardsCarousel/cardsCarousel'
 
 export default function Home() {
