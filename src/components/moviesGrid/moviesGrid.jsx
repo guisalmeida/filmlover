5b262f24125063cd8ccf60a612881eb963ff2f2a
guisalmeida <guisalmeida@gmail.com>
2023-08-11 21:42:47 +0100

    refactor: alert box

--- src/components/moviesGrid/moviesGrid.jsx
+++ src/components/moviesGrid/moviesGrid.jsx
@@ -4,15 +4,13 @@ import * as Styled from './moviesGrid.styled'
 
 export default function MoviesGrid({ movies, title }) {
   return (
-    <>
-      <Styled.MoviesGridContainer>
-        <h2 className='grid-title'>{title}</h2>
-        <Styled.MoviesGrid>
-          {movies.length ? movies.map(movie => (
-            <MovieCard key={movie.id} movie={movie} />
-          )) : <AlertBox>Nothing here!</AlertBox>}
-        </Styled.MoviesGrid>
-      </Styled.MoviesGridContainer>
-    </>
+    <Styled.MoviesGridContainer>
+      <h2 className='grid-title'>{title}</h2>
+      <Styled.MoviesGrid>
+        {movies.length ? movies.map(movie => (
+          <MovieCard key={movie.id} movie={movie} />
+        )) : <AlertBox show={true}>Nothing here!</AlertBox>}
+      </Styled.MoviesGrid>
+    </Styled.MoviesGridContainer>
   )
 }
